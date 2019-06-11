5c938fb5e246422a931dd9af838d64405b626aa0
GLProgrammer <jirka2002@gmail.com>
2019-06-11 19:34:38 +0200

    Basic terrain generator

---
+++ TerrainTools/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generator
+{
+    class Generator
+    {
+        public static float[,] RandomMap(int width, int height, string seed)
+        {
+            Random rand = new Random(seed.GetHashCode());
+            float[,] map = new float[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    map[x, y] = (float)rand.NextDouble();
+                }
+            }
+            return map;
+        }
+    }
+}
---
+++ Visualizer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Visualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] map = new float[10, 10];
+        }
+
+        private static void Visualize(float[,] map, int width, int height)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(map[x, y]);
+                }
+                Console.Write(System.Environment.NewLine);
+            }
+        }
+    }
+}
